043b8e84dfcfef52dc105f1a1c397e067808cfb4
Paul Tchouankam <messisazc31@gmail.com>
2023-06-05 10:15:28 +0200

    structure

--- praktikum1Malcherek/booking.cpp
+++
@@ -1,8 +0,0 @@
-#include "booking.h"
-
-
-
-booking::booking(std::string id, double price, std::string fromDate, std::string toDate): id(id), price(price), fromDtae(fromDate), toDate(toDate)
-{
-
-}
